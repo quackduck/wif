88d17294590a24fe3130196c603347c38f5037a6
Ishan Goel <igoel.mail@gmail.com>
2023-12-25 17:55:17 -0800

    track macs, cleanup tcp and ethernet

--- wif.c
+++ wif.c
@@ -9,66 +9,6 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-/* default snap length (maximum bytes per packet to capture) */
-#define SNAP_LEN 1518
-
-/* ethernet headers are always exactly 14 bytes [1] */
-#define SIZE_ETHERNET 14
-
-/* Ethernet addresses are 6 bytes */
-#define ETHER_ADDR_LEN	6
-
-/* Ethernet header */
-struct sniff_ethernet {
-        u_char  ether_dhost[ETHER_ADDR_LEN];    /* destination host address */
-        u_char  ether_shost[ETHER_ADDR_LEN];    /* source host address */
-        u_short ether_type;                     /* IP? ARP? RARP? etc */
-};
-
-/* IP header */
-struct sniff_ip {
-        u_char  ip_vhl;                 /* version << 4 | header length >> 2 */
-        u_char  ip_tos;                 /* type of service */
-        u_short ip_len;                 /* total length */
-        u_short ip_id;                  /* identification */
-        u_short ip_off;                 /* fragment offset field */
-        #define IP_RF 0x8000            /* reserved fragment flag */
-        #define IP_DF 0x4000            /* don't fragment flag */
-        #define IP_MF 0x2000            /* more fragments flag */
-        #define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
-        u_char  ip_ttl;                 /* time to live */
-        u_char  ip_p;                   /* protocol */
-        u_short ip_sum;                 /* checksum */
-        struct  in_addr ip_src,ip_dst;  /* source and dest address */
-};
-#define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
-#define IP_V(ip)                (((ip)->ip_vhl) >> 4)
-
-/* TCP header */
-typedef u_int tcp_seq;
-
-struct sniff_tcp {
-        u_short th_sport;               /* source port */
-        u_short th_dport;               /* destination port */
-        tcp_seq th_seq;                 /* sequence number */
-        tcp_seq th_ack;                 /* acknowledgement number */
-        u_char  th_offx2;               /* data offset, rsvd */
-#define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
-        u_char  th_flags;
-        #define TH_FIN  0x01
-        #define TH_SYN  0x02
-        #define TH_RST  0x04
-        #define TH_PUSH 0x08
-        #define TH_ACK  0x10
-        #define TH_URG  0x20
-        #define TH_ECE  0x40
-        #define TH_CWR  0x80
-        #define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
-        u_short th_win;                 /* window */
-        u_short th_sum;                 /* checksum */
-        u_short th_urp;                 /* urgent pointer */
-};
-
 void
 got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
 
@@ -186,18 +126,8 @@ got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 {
 
 	static int count = 1;                   /* packet counter */
-
-	/* declare pointers to packet headers */
-	// const struct sniff_ethernet *ethernet;  /* The ethernet header [1] */
-	// const struct sniff_ip *ip;              /* The IP header */
-	// const struct sniff_tcp *tcp;            /* The TCP header */
-	// const char *payload;                    /* Packet payload */
-
-	// int size_ip;
-	// int size_tcp;
-	// int size_payload;
-
-	//printf("%x %x %x %x\n", packet[0], packet[1], packet[2], packet[3]);
+	static char* macs[1000];
+	static int macs_count[1000];
 
 	u_char *curr = (u_char *)(packet + packet[2]); // skip radiotap
 	
@@ -208,79 +138,35 @@ got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 
 	printf("Length: %d\n", header->len);
 
-	printf("mac1: %s\n", mac_ntoa((u_char *)curr+4));
+	printf("mac1: %s\n", mac_ntoa((u_char *)curr+4)); // the receiver, which is what we want to track
 	printf("mac2: %s\n", mac_ntoa((u_char *)curr+6+4));
 	printf("mac3: %s\n", mac_ntoa((u_char *)curr+12+4));
 
 	print_payload(curr, header->len - packet[2]);
 
+	// tally up the packet length for each mac
+	for (int i = 0; i < 1000; i++)
+	{
+		if (macs[i] == NULL)
+		{
+			macs[i] = (char *)malloc(18);
+			strcpy(macs[i], mac_ntoa((u_char *)curr+4));
+			macs_count[i] = header->len - packet[2];
+			break;
+		}
+		else if (strcmp(macs[i], mac_ntoa((u_char *)curr+4)) == 0)
+		{
+			macs_count[i] += header->len - packet[2];
+			break;
+		}
+	}
+	// print out the macs and their tallies
+	for (int i = 0; i < 1000; i++)
+	{
+		if (macs[i] == NULL) break;
+		printf("%s: %d\n", macs[i], macs_count[i]);
+	}
 	return;
-
-	// /* define ethernet header */
-	// ethernet = (struct sniff_ethernet*)(packet);
-
-	// /* define/compute ip header offset */
-	// ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
-	// size_ip = IP_HL(ip)*4;
-	// if (size_ip < 20) {
-	// 	printf("   * Invalid IP header length: %u bytes\n", size_ip);
-	// 	// return;
-	// }
-
-	// /* print source and destination IP addresses */
-	// printf("       From: %15s %s\n", inet_ntoa(ip->ip_src), mac_ntoa(ethernet->ether_shost));
-	// printf("         To: %15s %s\n", inet_ntoa(ip->ip_dst), mac_ntoa(ethernet->ether_dhost));
-
-	// /* determine protocol */
-	// switch(ip->ip_p) {
-	// 	case IPPROTO_TCP:
-	// 		printf("   Protocol: TCP\n");
-	// 		break;
-	// 	case IPPROTO_UDP:
-	// 		printf("   Protocol: UDP\n");
-	// 		return;
-	// 	case IPPROTO_ICMP:
-	// 		printf("   Protocol: ICMP\n");
-	// 		return;
-	// 	case IPPROTO_IP:
-	// 		printf("   Protocol: IP\n");
-	// 		return;
-	// 	default:
-	// 		printf("   Protocol: unknown\n");
-	// 		return;
-	// }
-
-	// /*
-	//  *  OK, this packet is TCP.
-	//  */
-
-	// /* define/compute tcp header offset */
-	// tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
-	// size_tcp = TH_OFF(tcp)*4;
-	// if (size_tcp < 20) {
-	// 	printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
-	// 	return;
-	// }
-
-	// printf("   Src port: %d\n", ntohs(tcp->th_sport));
-	// printf("   Dst port: %d\n", ntohs(tcp->th_dport));
-
-	// /* define/compute tcp payload (segment) offset */
-	// payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
-
-	// /* compute tcp payload (segment) size */
-	// size_payload = ntohs(ip->ip_len) - (size_ip + size_tcp);
-
-	// /*
-	//  * Print payload data; it might be binary, so don't just
-	//  * treat it as a string.
-	//  */
-	// if (size_payload > 0) {
-	// 	printf("   Payload (%d bytes):\n", size_payload);
-	// 	print_payload(payload, size_payload);
-	// }
-
-return;
 }
 
 int main()
@@ -341,7 +227,7 @@ int main()
 	{
 		printf("monitor mode enabled\n");
 	}
-	pcap_set_snaplen(handle, 1518);  // Set the snapshot length to 2048
+	pcap_set_snaplen(handle, 65535);  // Set the snapshot length to 65535
 	pcap_set_promisc(handle, 1); // Turn promiscuous mode off
 	pcap_set_timeout(handle, 512); // Set the timeout to 512 milliseconds
 	int status = pcap_activate(handle);
